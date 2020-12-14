f1cf8e65b291a182276673fbbab9186067d17ca5
Keolai <69029671+Keolai@users.noreply.github.com>
2020-12-14 09:08:07 -0600

    script now checks for element transformation

--- MeetBot/content.js
+++ MeetBot/content.js
@@ -4,19 +4,21 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
     var check = document.getElementsByClassName('uArJ5e UQuaGc Y5sE8d uyXBBb nS35F M9Bg4d');
     if (check.length > 0) {
         var Message = "bad: ";
+        sendResponse(Message+JSON.stringify("request"));
     } else {
         var Message = "firing: ";
+        sendResponse(Message+JSON.stringify("request"));
         main(); 
     }
-    sendResponse(Message+JSON.stringify("request"));
 }); 
 function main(){
+     var clickEvent = false; 
      var joinButton;
      onLoad(); 
     
     function onLoad(){
        joinButton = document.getElementsByClassName('NPEfkd RveJvd snByac');
-      }
+  }
 
     if (joinButton.length < 2){
        setTimeout(onLoad, 2000); 
@@ -24,7 +26,7 @@ function main(){
         click();
     }
 
-function click(){
+    function click(){
     
     var settings = document.getElementsByClassName('x95qze');
     if (settings.length > 0 ){ //checks to see if popup exists
@@ -37,12 +39,29 @@ function click(){
 
     document.getElementsByClassName('DPvwYc JnDFsc dMzo5')[0].click(); 
     document.getElementsByClassName('DPvwYc JnDFsc dMzo5')[1].click(); 
-
-    function join(){
-     document.getElementsByClassName('NPEfkd RveJvd snByac')[0].click(); 
+    
+    direct(); 
+  }
+  
+   function direct(){
+    clickJoin(); 
+    setTimeout(check,3000); 
+  }
+    function clickJoin(){
+    //p2hjYe TPpRNe
+   
+        function join(){
+        document.getElementsByClassName('NPEfkd RveJvd snByac')[0].click(); 
     }
-
+    
     setTimeout(join, 1000); 
-
-    } 
+    
+  } 
+function check(){
+var video = document.getElementsByClassName('p2hjYe TPpRNe');
+if (video.length < 2){ 
+    console.log("trying again"); 
+    direct(); 
+    }
+  }
 }
